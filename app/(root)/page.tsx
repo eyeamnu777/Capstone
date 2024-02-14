751cb6c6101e25b0b4b51c7049fcf5984d953d2e
eyeamnu777 <sharley777@yahoo.com>
2024-02-13 21:35:38 -0500

    3:24 marker in video

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button";
-import { Button } from '@/components/ui/button'
+import { Image } from "next/image";
 
 export default function Home() {
   return (
     <main>
-     
+      <h1>Prezently</h1>
     </main>
-  )
+  );
 }
